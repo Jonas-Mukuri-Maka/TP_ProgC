383de2062929ca26434f5ba7445185b2023c3258
Jonas-Mukuri-Maka <114100035+Jonas-Mukuri-Maka@users.noreply.github.com>
2025-12-01 10:26:48 +0100

    Exercise 1.6.2

--- TP1/src/boucles.c
+++ TP1/src/boucles.c
@@ -1,3 +1,4 @@
+/*
 #include <stdio.h>
 
 int main() {
@@ -29,6 +30,49 @@ int main() {
 
     return 0;
 }
+*/
+
+#include <stdio.h>
+
+int main() {
+    int compteur = 5;
+
+    if (compteur >= 10) {
+        printf("Erreur : compteur doit etre strictement inferieur a 10.\n");
+        return 1;
+    }
+    int i = 1;
+    while (i <= compteur) {
+        int j = 1;
+        while (j <= i) {
+
+            if (j > 20) break;
+            // Lignes 1, 2 et dernière ligne *
+            if (i == 1 || i == 2 || i == compteur) {
+                printf("* ");
+            }
+            else { 
+                // Lignes du milieu #
+                if (j == 1 || j == i)
+                    printf("* ");
+                else
+                    printf("# ");
+            }
+
+            j++;
+        }
+        printf("\n");
+        // Démonstration : si i == 8 → continue (sauter ligne 8)
+        if (i == 8) {
+            i++;
+            continue;
+        }
+        i++;
+    }
+
+    return 0;
+}
+
 
 
 
