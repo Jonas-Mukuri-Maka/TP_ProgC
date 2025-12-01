6f2ed846578759cc21b67e22c89b119825b988d2
Jonas-Mukuri-Maka <114100035+Jonas-Mukuri-Maka@users.noreply.github.com>
2025-12-01 10:12:46 +0100

    Exercise 1.6

---
+++ TP1/src/boucles.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+  int compteur = 5; 
+
+  if (compteur >= 10) {
+      printf("Erreur : compteur doit etre strictement inferieur a 10.\n");
+      return 1;
+  }
+
+  for (int i = 1; i <= compteur; i++) {
+      for (int j = 1; j <= i; j++) {
+          if (i == 1)           printf("* ");
+          else if (i == 2)      printf("* ");
+          else if (i == 3 && j > 1) printf("# ");
+          else if (i == 4 && j > 1) printf("# ");
+          else if (i == 5)      printf("* ");
+          else                  printf("* ");
+      }
+      printf("\n");
+  }
+
+  return 0;
+}
