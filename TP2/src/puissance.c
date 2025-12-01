f6a805dfd9c9f9cb24c869865cb733003beda579
Jonas-Mukuri-Maka <114100035+Jonas-Mukuri-Maka@users.noreply.github.com>
2025-12-01 12:34:22 +0100

    Exercise 2.1

--- TP2/src/puissance.c
+++ TP2/src/puissance.c
@@ -1,26 +1,28 @@
 #include <stdio.h>
-#include <math.h>
 
 int main() {
   int a = 2;
   int b = 3;
-
   int resultat;
-
-  if (a == 0)
+  // Si b est égale a 0, le resultat est toujours 1
+  if (b == 0)
+  {
+    resultat = 1;
+  }
+  // Si a est égale a 0, le resultat ne peut que etre 0 (sauf pour 0^^0)
+  else if (a == 0)
   {
     resultat = 0;
   }
-  else if (b == 0)
+  else
   {
-    resultat = 0;
-  }
-  else{
+    // Boucle pour multiplier a par lui meme b fois
+    resultat = 1;
     for (int i = 1; i <= b; i++) 
     {
-    resultat = resultat * a;
+      resultat = resultat * a;
     }
   }
-
-  printf("a^b = ", resultat);
+  printf("a^b = %d\n", resultat);
 }
+
