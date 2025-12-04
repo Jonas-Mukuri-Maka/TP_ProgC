1071db806a5410bcde0c661caa0bb9338c944861
Jonas-Mukuri-Maka <114100035+Jonas-Mukuri-Maka@users.noreply.github.com>
2025-12-04 16:19:44 +0100

    Exercice 3.1

---
+++ TP3/src/sizeof.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int main() {
+  
+  printf("Taille de int : %zu octets\n", sizeof(int));
+  printf("Taille de int* : %zu octets\n", sizeof(int*));
+  printf("Taille de int** : %zu octets\n", sizeof(int**));
+  printf("Taille de char : %zu octets\n", sizeof(char));
+  printf("Taille de char* : %zu octets\n", sizeof(char*));
+  printf("Taille de char** : %zu octets\n", sizeof(char**));
+  printf("Taille de char*** : %zu octets\n", sizeof(char***));
+  printf("Taille de float : %zu octets\n", sizeof(float));
+  printf("Taille de float* : %zu octets\n", sizeof(float*));
+  printf("Taille de float** : %zu octets\n", sizeof(float**));
+  printf("Taille de float*** : %zu octets\n", sizeof(float***));
+  printf("Taille de long : %zu octets\n", sizeof(long));
+  printf("Taille de long* : %zu octets\n", sizeof(long*));
+  printf("Taille de long** : %zu octets\n", sizeof(long**));
+  printf("Taille de long*** : %zu octets\n", sizeof(long***));
+  printf("Taille de long long : %zu octets\n", sizeof(long long));
+  printf("Taille de long long* : %zu octets\n", sizeof(long long*));
+  printf("Taille de long long** : %zu octets\n", sizeof(long long**));
+  printf("Taille de long long*** : %zu octets\n", sizeof(long long***));
+  printf("Taille de double : %zu octets\n", sizeof(double));
+  printf("Taille de double* : %zu octets\n", sizeof(double*));
+  printf("Taille de double** : %zu octets\n", sizeof(double**));
+  printf("Taille de double*** : %zu octets\n", sizeof(double***));
+  printf("Taille de long double : %zu octets\n", sizeof(long double));
+  printf("Taille de long double* : %zu octets\n", sizeof(long double*));
+  printf("Taille de long double** : %zu octets\n", sizeof(long double**));
+  printf("Taille de long double*** : %zu octets\n", sizeof(long double***));
+
+  
+
+  return 0;
+}
