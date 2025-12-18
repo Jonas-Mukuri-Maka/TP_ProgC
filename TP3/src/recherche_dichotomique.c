2be6ebc71f4af0a8e73b2804ee6da5b65ce6c8f8
Jonas-Mukuri-Maka <114100035+Jonas-Mukuri-Maka@users.noreply.github.com>
2025-12-18 19:32:25 +0100

    Exercice 3.5

--- TP3/src/recherche_dichotomique.c
+++ TP3/src/recherche_dichotomique.c
@@ -2,40 +2,44 @@
 #include <stdlib.h>
 #include <time.h>
 
-
 int main() {
     int Liste[100];
 
     srand(time(NULL));
 
-    // Remplissage du Liste avec des valeurs croissantes avec chaque élément > précédent
+    // Valeur initiale
     Liste[0] = (rand() % 201) - 100;
+
+    // Remplissage strictement croissant
     for (int i = 1; i < 100; i++) {
-        Liste[i] = Liste[i - 1] + (rand() % 201) - 100;
+        Liste[i] = Liste[i - 1] + (rand() % 10 + 1);
     }
 
-    // Affichage du Liste trié
+    // Affichage
     printf("Tableau trié :\n");
     for (int i = 0; i < 100; i++) {
         printf("%d ", Liste[i]);
     }
     printf("\n\n");
 
-    // Saisie de l'entier à rechercher
+    // Saisie
     int numCherche;
     printf("Entrez l'entier que vous souhaitez chercher : ");
     scanf("%d", &numCherche);
 
     // Recherche dichotomique
-    int debut = 0, fin = 100 - 1;
+    int debut = 0, fin = 99;
     while (debut <= fin) {
         int milieu = (debut + fin) / 2;
+
         if (Liste[milieu] == numCherche) {
             printf("Résultat : entier présent\n");
             return 0;
-        } else if (Liste[milieu] < numCherche) {
+        } 
+        else if (Liste[milieu] < numCherche) {
             debut = milieu + 1;
-        } else {
+        } 
+        else {
             fin = milieu - 1;
         }
     }
@@ -43,5 +47,3 @@ int main() {
     printf("Résultat : entier absent\n");
     return 0;
 }
-
-
