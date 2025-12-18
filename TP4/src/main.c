0c8eeff087f21076fd569e2780c661293c2101d5
Jonas-Mukuri-Maka <114100035+Jonas-Mukuri-Maka@users.noreply.github.com>
2025-12-18 19:53:27 +0100

    Exercice 4.1

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,1 +1,50 @@
+#include <stdio.h>
+#include "operator.h"
 
+int main() {
+    int num1, num2, resultat;
+    char op;
+
+    printf("Entrez num1 : ");
+    scanf("%d", &num1);
+
+    printf("Entrez num2 : ");
+    scanf("%d", &num2);
+
+    printf("Entrez l'operateur (+, -, *, /, %%, &, |, ~) : ");
+    scanf(" %c", &op);
+
+    switch (op) {
+        case '+':
+            resultat = somme(num1, num2);
+            break;
+        case '-':
+            resultat = difference(num1, num2);
+            break;
+        case '*':
+            resultat = produit(num1, num2);
+            break;
+        case '/':
+            resultat = quotient(num1, num2);
+            break;
+        case '%':
+            resultat = modulo(num1, num2);
+            break;
+        case '&':
+            resultat = et(num1, num2);
+            break;
+        case '|':
+            resultat = ou(num1, num2);
+            break;
+        case '~':
+            resultat = negation(num1);
+            break;
+        default:
+            printf("Operateur invalide\n");
+            return 0;
+    }
+
+    printf("Resultat : %d\n", resultat);
+
+    return 0;
+}
