f8d6a083349024f956b894723a2e0a831c16615a
Jonas-Mukuri-Maka <114100035+Jonas-Mukuri-Maka@users.noreply.github.com>
2025-12-18 20:18:09 +0100

    Exercice 4.2

--- TP4/src/main.c
+++ TP4/src/main.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include "operator.h"
-
+#include "fichier.h"
 int main() {
+    
+    printf("Choisissez l'exercice a executer:\n");
+    printf("1. operateur\n");
+    printf("2. fichier\n");
+    printf("3. Exercice 4.7 (gestion de fichiers)\n");
+    printf("Votre choix : ");
+    scanf("%d", &choix);
+
+    switch (choix) {
+        case 1:
+            operateur();
+            break;
+        case 2:
+            fichier();
+            break;
+        case 3:
+            liste();
+            break;
+        default:
+            printf("Choix invalide\n");
+}
+
+void operateur() {
     int num1, num2, resultat;
     char op;
 
@@ -43,8 +66,41 @@ int main() {
             printf("Operateur invalide\n");
             return 0;
     }
+}
 
-    printf("Resultat : %d\n", resultat);
+void fichier() {
+    int choix;
+    char nom_fichier[100];
+    char message[256];
 
-    return 0;
+    printf("Que souhaitez-vous faire ?\n");
+    printf("1. Lire un fichier\n");
+    printf("2. Ecrire dans un fichier\n");
+    printf("Votre choix : ");
+    scanf("%d", &choix);
+
+    getchar(); // retire le \n restant dans le buffer
+
+    if (choix == 1) {
+        printf("Entrez le nom du fichier a lire : ");
+        scanf("%99s", nom_fichier);
+        lire_fichier(nom_fichier);
+    } 
+    else if (choix == 2) {
+        printf("Entrez le nom du fichier dans lequel vous souhaitez ecrire : ");
+        scanf("%99s", nom_fichier);
+
+        getchar(); // retire le \n
+        printf("Entrez le message a ecrire : ");
+        fgets(message, 256, stdin);
+
+        ecrire_dans_fichier(nom_fichier, message);
+    } 
+    else {
+        printf("Choix invalide\n");
+    }
+}
+
+void liste() {
+    exit(0);
 }
